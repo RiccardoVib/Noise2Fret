5a6a3a4a667118dbca947182ae49ae1c5558148b
Riccardo <riccardo.simionato.vib@gmail.com>
2026-08-11 13:13:26 +0200

    Update GuitarSetDataset.py

--- src/GuitarSetDataset.py
+++ src/GuitarSetDataset.py
@@ -1,23 +1,9 @@
-# 1. Compute full CQT  →  (n_frames, 192)
-# 2. Convert JAMS annotations to frame indices  →  (n_frames, 6, 21)
-# 3. For each frame k: slice CQT[k-4:k+5]  →  store as repr[k]  shape (9, 192)
-# 4. Transpose  →  (192, 9)
-# 5. labels[k] = tab_matrix[k]
-# 6. Save both arrays together in .npz
-
-
 import torch
 from torch.utils.data import Dataset
 import numpy as np
 import os
 from pathlib import Path
 from utils import find_folder_upward
-
-current_dir = Path(os.getcwd())
-print(f"current_dir: {current_dir}")
-files_dir = find_folder_upward(folder_name="Files", start_path=current_dir)
-npz_dir = files_dir / "GuitarSet/c/"
-
 
 class TabDataset(Dataset):
     def __init__(self, npz_dir):
